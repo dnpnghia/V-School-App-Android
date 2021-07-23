894af51055d5c9da00474beb38e5cb1d120e2c7a
nghiaaaaa <idnpnlol@gmail.com>
2021-07-23 17:23:04 +0700

    set is read

--- app/src/main/java/com/example/v_school/NotificationAdapter.java
+++ app/src/main/java/com/example/v_school/NotificationAdapter.java
@@ -48,6 +48,7 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sanphamList.get(position).setIsRead(1);
                 myOnClickItemListener.onClick(sanphamList.get(position));
             }
         });
